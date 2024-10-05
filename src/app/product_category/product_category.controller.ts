5ffe3df3b026a59a687bbd7a535efb8cda274d8f
thomasalbertoDev <12221355@bsi.ac.id>
2024-10-05 12:39:05 +0700

    feat: find all product category API completed

--- src/app/product_category/product_category.controller.ts
+++ src/app/product_category/product_category.controller.ts
@@ -6,9 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductCategoryService } from './product_category.service';
-import { ProductCategoryRequestDto } from './dtos/product_category.dto';
+import {
+  ProductCategoryRequestDto,
+  ProductCategoryResponseDto,
+} from './dtos/product_category.dto';
+import { AdminGuard } from 'src/common/guards/admin.guard';
+import { AuthGuard } from 'src/common/guards/auth.guard';
 
 @Controller('/api/product-category')
 export class ProductCategoryController {
@@ -17,9 +24,24 @@ export class ProductCategoryController {
   ) {}
 
   @Post('/create')
+  @UseGuards(AdminGuard, AuthGuard)
   async createProductCategoryController(
     @Body() request: ProductCategoryRequestDto,
   ) {
     return this.productCategoryService.createProductCategoryService(request);
+  }
+
+  @Get('/all')
+  @UseGuards(AdminGuard, AuthGuard)
+  async findAllProductCategoryController(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+  ): Promise<IBaseResponse<ProductCategoryResponseDto[]>> {
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
+    return this.productCategoryService.findAllProductCategoryService(
+      pageNumber,
+      limitNumber,
+    );
   }
 }
