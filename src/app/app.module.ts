49234ea55bd9fdcf41c2b4af19b07447fa416a05
thomasalbertoDev <12221355@bsi.ac.id>
2024-10-07 12:24:08 +0700

    chore: generate resource product API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { RoleModule } from './role/role.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductCategoryModule } from './product_category/product_category.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [RoleModule, UsersModule, AuthModule, ProductCategoryModule],
+  imports: [RoleModule, UsersModule, AuthModule, ProductCategoryModule, ProductModule],
   providers: [],
 })
 export class AppModule {}
