05f155645415b909d82f77d61e9c2c47c7a502dc
thomasalbertoDev <12221355@bsi.ac.id>
2024-10-11 13:38:54 +0700

    chore: delete constuctor unimportant

--- src/app/orders/repositories/orders.repository.ts
+++ src/app/orders/repositories/orders.repository.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { IOrdersRepository } from '../interfaces/orders.interface';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Products } from 'src/app/products/entities/products.entity';
 import { DeleteResult, Repository } from 'typeorm';
 import { Orders } from '../entities/orders.entity';
-import { Carts } from 'src/app/carts/entities/carts.entity';
+import { IOrdersRepository } from '../interfaces/orders.interface';
 
 @Injectable()
 export class OrdersRepository implements IOrdersRepository {
   constructor(
     @InjectRepository(Orders)
     private readonly ordersRepository: Repository<Orders>,
-    @InjectRepository(Products)
-    private readonly productsRepository: Repository<Products>,
-    @InjectRepository(Carts)
-    private readonly cartsRepository: Repository<Carts>,
   ) {}
 
   async createOrders(data: Orders[]): Promise<Orders[]> {
