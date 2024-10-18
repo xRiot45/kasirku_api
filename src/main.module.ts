79afc7cbe7c9d6b2db0521e8ba2f4cdb229fb5b3
thomasalbertoDev <12221355@bsi.ac.id>
2024-10-18 11:51:14 +0700

    add limit request

--- src/main.module.ts
+++ src/main.module.ts
@@ -16,17 +16,17 @@ import * as config from 'src/configs/environment.config';
       {
         // 10 requests per minute
         ttl: 60,
-        limit: 10,
+        limit: 100,
       },
       {
         // 100 requests per hour
         ttl: 3600,
-        limit: 100,
+        limit: 1000,
       },
       {
         // 1000 requests per day
         ttl: 86400,
-        limit: 1000,
+        limit: 10000,
       },
     ]),
     WinstonModule.forRoot(logger),
