5154abaf68c847056d319dc17abc7159f8630dbf
thomasalbertoDev <12221355@bsi.ac.id>
2024-11-04 10:10:08 +0700

    feat: generate invoice number

--- src/common/helpers/generateInvoice.helper.ts
+++ src/common/helpers/generateInvoice.helper.ts
@@ -1,8 +1,6 @@
-import cuid from 'cuid';
-
 export const generateInvoiceNumber = () => {
   const datePart = new Date().toISOString().slice(0, 10).replace(/-/g, '');
-  const cuidPart = cuid();
+  const randomPart = Math.random().toString(36).substring(2, 10).toUpperCase();
 
-  return `INV-${datePart}-${cuidPart}`;
+  return `INV-${datePart}-${randomPart}`;
 };
